838d775f7995c8f0c536773b20d2e43b447fad14
Андрей Кан <1kan_andreikkkk@mail.ru>
2020-09-20 12:49:27 +0600

    HomeWork

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HW_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var colletion = new List<string>
+            {
+                "Shekspir","Tom Kruz","Never to Die"
+            };
+            var selectedBook = new List<string>();
+            foreach (var book in colletion)
+            {
+                if (book.ToLower().Contains("a"))
+                    selectedBook.Add(book);
+            }
+
+            var plainResult = from book in colletion where book.ToLower().Contains("a") select book;
+        }
+    }
+}
---
+++ StringExtantion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_17
+{
+    public static class StringExtantion
+    {
+        public static int ParseData(this string data)
+        {
+            int number;
+
+            if (int.TryParse(data, out number))
+            {
+                return number;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
